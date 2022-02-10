f6c4b807505adfef956140df6e43550f64f311c2
anonyblast <gustavoiafelix@gmail.com>
2022-02-09 21:41:00 -0300

    Class 01 Content

---
+++ README.md
@@ -0,0 +1,12 @@
+# ACAMPAMENTO PYTHON (GRUPO ÂNIMA)
+
+### AULA 01
+> Assuntos abordados 
+- História da Programação
+- Linguagens de Programação (Popularidade, Utilidade e Diferenças)
+- Sobre o Python
+- Síntaxe Básica :
+    - Operações matemáticas básicas
+    - Escrita de texto
+    - print
+    - input
---
+++ aula_py_01/teste01.py
@@ -0,0 +1,24 @@
+# Conteúdo praticado durante a primeira aula de Python
+
+
+# Contéudo demonstrado pelo instrutor
+print('Hello, World!')
+print("Eu estou dentro do Acampamento Python!")
+
+x = input("Digite algo: ")
+
+print(x)
+
+
+# Código escrito além da aula (Teste)
+""" def soma(numero1, numero2):
+    return int(numero1) + int(numero2)
+
+
+x = int(input('Insert first number: \n'))
+y = int(input('Insert second number: \n'))
+
+if(soma(x, y) > 5):
+    print('Correct values')
+else:
+    print('Please, only numbers that add up to more than five') """
